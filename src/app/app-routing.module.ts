64b78e884d31accbd282f16a4e1cb01bf5bde80c
Safwan <safwan9f@gmail.com>
2020-11-16 09:14:53 +0300

    Finish header with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticlesComponent } from './articles/articles.component';
+import { HomeComponent } from './home/home.component';
+import { IssuesComponent } from './issues/issues.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SubmitIssueComponent } from './submitIssue/submitIssue.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'articles', component: ArticlesComponent},
+  {path: 'issues', component: IssuesComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'submitIssue', component: SubmitIssueComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
