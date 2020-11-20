32d7bd46cd395a56a8c73ab3cd74dad2f1cac7a9
Safwan <safwan9f@gmail.com>
2020-11-21 01:08:08 +0300

    Implement add article and discard one

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Timestamp } from 'rxjs/internal/operators/timestamp';
+import { Article, ArticleService } from 'src/app/services/article.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  isProcessing = true;
+  contentString: string;
+
+  constructor(
+    private fb: FormBuilder,
+    private articleService: ArticleService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      content: ['', Validators.required]
+    });
+  }
+
+  get title(): any {
+    return this.form.get('title');
+  }
+
+  get description(): any {
+    return this.form.get('description');
+  }
+
+  get content(): any {
+    return this.form.get('content');
+  }
+
+  onSubmit(): void {
+    this.isProcessing = true;
+
+    this.authService.user$.subscribe(user => {
+      const t = new Date();
+      this.articleService.save(new Article(
+        `${user.uid}-${t.getTime()}`,
+        this.title.value,
+        this.description.value,
+        this.contentString,
+        new Date(),
+        0,
+        user.uid
+      )).then(_ => {
+        this.isProcessing = false;
+      });
+    });
+  }
+
+  discard(): void {
+    this.router.navigateByUrl('/articles');
   }
 
 }
