61e6584078d3b8852e98bd5aec036b550956be65
Safwan <safwan9f@gmail.com>
2020-11-22 23:03:50 +0300

    Implement the search in both the user and the articles

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -14,20 +14,38 @@ export class ArticlesComponent implements OnInit {
   articles: Article[];
   isWriter: boolean[] = [];
   form: FormGroup;
+  filteredArticles: Article[];
 
   constructor(private articleService: ArticleService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      search: ['']
+      searchText: ['']
     });
     // this.articleService.addDummyData().then();
-    this.articleService.getAllArticles().subscribe(articles => { this.articles = articles; this.setIsWriter(); });
+    this.articleService.getAllArticles().subscribe(articles => {
+      this.articles = articles;
+      this.filteredArticles = this.articles;
+      this.setIsWriter();
+    });
+  }
+
+  get searchText(): any {
+    return this.form.get('searchText');
   }
 
   setIsWriter(): void {
     for (const article of this.articles) {
       this.articleService.isWriter(article).subscribe(result => this.isWriter.push(result));
     }
+  }
+
+  onSearch(): void {
+    this.filteredArticles = this.articles.filter(article => {
+      if (this.searchText.value === '') { return true; }
+      return article.description.split(' ').includes(this.searchText.value) ||
+        article.title.split(' ').includes(this.searchText.value);
+    }
+    );
   }
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -12,14 +12,31 @@ export class UsersComponent implements OnInit {
   form: FormGroup;
   isProcessed = false;
   users: User[];
+  filteredUsers: User[];
 
   constructor(private userService: UserService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      search: ['']
+      searchText: ['']
     });
-    this.userService.getAllUsers().subscribe(users => this.users = users);
+    this.userService.getAllUsers().subscribe(users => {
+      this.users = users;
+      this.filteredUsers = this.users;
+    });
+  }
+
+  get searchText(): any {
+    return this.form.get('searchText');
+  }
+
+  onSearch(): void {
+    this.filteredUsers = this.users.filter(user => {
+      if (this.searchText.value === '') { return true; }
+      return user.firstName.startsWith(this.searchText.value) ||
+        user.lastName.startsWith(this.searchText.value);
+    }
+    );
   }
 
 }
