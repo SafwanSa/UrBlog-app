a39401876777ee75b6724470323584267fda4200
Fozan Alkhalawi <FozanAlkhalawi@gmail.com>
2020-12-11 23:25:15 +0300

    implement resposive header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,18 +9,26 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
   isProcessing = true;
   user: User;
+  collapsed = true;
 
-  constructor(public authService: AuthService, private router: Router, private userService: UserService) { }
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private userService: UserService
+  ) {}
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
 
   ngOnInit(): void {
     this.isProcessing = true;
-    this.user$.subscribe(user => {
+    this.user$.subscribe((user) => {
       if (user) {
         this.user = user;
       } else {
@@ -28,20 +36,25 @@ export class HeaderComponent implements OnInit {
       }
       this.isProcessing = false;
     });
-
   }
 
   get user$(): Observable<User | null> {
-    return this.authService.user$.pipe(switchMap(authUser => {
-      if (!authUser) { return of(null); }
-      return this.userService.get$(authUser.uid).pipe(map(user => {
-        if (user) {
-          return user;
+    return this.authService.user$.pipe(
+      switchMap((authUser) => {
+        if (!authUser) {
+          return of(null);
         }
-        this.isProcessing = false;
-        return null;
-      }));
-    }));
+        return this.userService.get$(authUser.uid).pipe(
+          map((user) => {
+            if (user) {
+              return user;
+            }
+            this.isProcessing = false;
+            return null;
+          })
+        );
+      })
+    );
   }
 
   async signOut(): Promise<void> {
@@ -52,5 +65,4 @@ export class HeaderComponent implements OnInit {
     }
     this.router.navigateByUrl('/');
   }
-
 }
