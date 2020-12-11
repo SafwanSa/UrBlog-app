f26df0a93a100c86b5212f021f043a2448a980a7
Safwan <safwan9f@gmail.com>
2020-12-11 22:32:37 +0300

    add global var, implement new search

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { FirestoreService } from 'src/app/services/firestore/firestore.service';
 import { User, UserService } from 'src/app/services/user.service';
+import * as g from '../../global';
 
 @Component({
   selector: 'app-articles',
@@ -16,20 +17,25 @@ export class ArticlesComponent implements OnInit {
   isWriter: boolean[] = [];
   users: User[] = [];
   form: FormGroup;
-  filteredArticles: Article[];
+  // filteredArticles: Article[];
   selectedTag = '';
+  searchTerm = '';
 
   constructor(private articleService: ArticleService, private fb: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
+    g.setValue(true);
     this.form = this.fb.group({
       searchText: ['']
     });
     this.articleService.getAllArticles().subscribe(articles => {
+      if (!articles) { g.setValue(false); }
       this.userService.getAll$().subscribe(users => {
+        if (!users) { g.setValue(false); }
         this.users = users;
         this.articles = articles;
-        this.filteredArticles = this.articles;
+        // this.filteredArticles = this.articles;
+        g.setValue(false);
         this.setIsWriter();
       });
     });
@@ -51,35 +57,59 @@ export class ArticlesComponent implements OnInit {
     }
   }
 
-  onSearch(): void {
-    console.log(this.searchText.value);
-    console.log(this.selectedTag, ' tag');
-
-    this.filteredArticles = this.articles.filter(article => {
-      const articleCopy = new Article(
-        article.id,
-        article.title,
-        article.description,
-        article.content,
-        article.date,
-        article.rating,
-        article.uid,
-        article.tag,
-        article.image,
-      );
-
-      if (this.searchText.value === '') { return true; }
-      articleCopy.title = articleCopy.title.toLowerCase();
-      articleCopy.description = articleCopy.description.toLowerCase();
-      this.searchText.value = this.searchText.value.toLowerCase();
-      if (this.selectedTag === '') {
-        return (articleCopy.title.startsWith(this.searchText.value) || (articleCopy.description.startsWith(this.searchText.value)));
-      } else {
-        return ((articleCopy.title.startsWith(this.searchText.value) ||
-          (articleCopy.description.startsWith(this.searchText.value)))) &&
-          (this.selectedTag === articleCopy.tag);
-      }
+  get filtered(): Article[] {
+    if (!this.articles) {
+      return [];
+    } else {
+      return this.articles.filter(article => {
+        if (this.searchTerm !== '') {
+          if (this.selectedTag === '') {
+            return (article.title.toLocaleLowerCase().startsWith(this.searchTerm.toLocaleLowerCase()) ||
+              article.description.toLocaleLowerCase().startsWith(this.searchTerm.toLocaleLowerCase()));
+          } else {
+            return (article.title.toLocaleLowerCase().startsWith(this.searchTerm.toLocaleLowerCase()) ||
+              article.description.toLocaleLowerCase().startsWith(this.searchTerm.toLocaleLowerCase())) &&
+              article.tag === this.selectedTag;
+          }
+        }
+        if (this.selectedTag !== '') {
+          return article.tag === this.selectedTag;
+        }
+        return true;
+      });
     }
-    );
+
   }
+
+  // onSearch(): void {
+  //   console.log(this.searchText.value);
+  //   console.log(this.selectedTag, ' tag');
+
+  //   this.filteredArticles = this.articles.filter(article => {
+  //     const articleCopy = new Article(
+  //       article.id,
+  //       article.title,
+  //       article.description,
+  //       article.content,
+  //       article.date,
+  //       article.rating,
+  //       article.uid,
+  //       article.tag,
+  //       article.image,
+  //     );
+
+  //     if (this.searchText.value === '') { return true; }
+  //     articleCopy.title = articleCopy.title.toLowerCase();
+  //     articleCopy.description = articleCopy.description.toLowerCase();
+  //     this.searchText.value = this.searchText.value.toLowerCase();
+  //     if (this.selectedTag === '') {
+  //       return (articleCopy.title.startsWith(this.searchText.value) || (articleCopy.description.startsWith(this.searchText.value)));
+  //     } else {
+  //       return ((articleCopy.title.startsWith(this.searchText.value) ||
+  //         (articleCopy.description.startsWith(this.searchText.value)))) &&
+  //         (this.selectedTag === articleCopy.tag);
+  //     }
+  //   }
+  //   );
+  // }
 }
